6a533dc7c89a6e160fd5ba4c6f40881158202673
alfisjahri <alfisjahri@gmail.com>
2026-07-01 10:12:52 +0800

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,6 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
+      <head>
+        {/* Pintu masuk resmi buat panah dan ikon-ikon web lu */}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" precedence="default" />
+      </head>
       <body>
         {children} 
       </body>
