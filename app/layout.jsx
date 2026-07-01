e232f012b9be5d9d83814c688a7c4ce7f2d534f1
alfisjahri <alfisjahri@gmail.com>
2026-07-01 12:40:59 +0800

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,18 @@
 import './globals.css'
 
+// Di sinilah KTP Google lu sekarang ditanam secara otomatis sama Next.js!
 export const metadata = {
   title: 'SMP Negeri 1 Damai',
-  description: 'Membentuk generasi unggul, beradab, dan melek teknologi',
+  description: 'Membentuk generasi unggul, beradab, dan melek teknologi untuk masa depan gemilang.',
+  verification: {
+    google: '4S6p9Q42VifIpP5fYTyuVVhnG-7prq52Ibtf1cdEObY',
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
       <head>
-        {/* Pintu masuk resmi buat panah dan ikon-ikon web lu */}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" precedence="default" />
       </head>
       <body>
