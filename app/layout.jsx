a501f0a892e538d352dc3008985a1f9ce575d694
alfisjahri <alfisjahri@gmail.com>
2026-07-01 10:01:22 +0800

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,9 +8,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
-      <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
-      </head>
       <body>
         {children} 
       </body>
