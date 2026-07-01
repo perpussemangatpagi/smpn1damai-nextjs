b10dbcbd77bd2dd91034630b74d70419a22c778f
alfisjahri <alfisjahri@gmail.com>
2026-07-01 14:35:57 +0800

    Update ClientPage.jsx

--- app/ClientPage.jsx
+++ app/ClientPage.jsx
@@ -175,7 +175,6 @@ export default function ClientPage({ berita, settings }) {
                 <button onClick={bagikanBerita} style={{background:'#10b981', color:'white', border:'none', padding:'8px 15px', borderRadius:'12px', fontWeight:'bold', cursor:'pointer'}}><i className="fa-solid fa-share-nodes"></i> Bagikan</button>
               </div>
               <div <div className="text-content" dangerouslySetInnerHTML={{ __html: activeModal.body.replace(/!\[(.*?)\]\((.*?)\)/g, '<img src="$2" alt="$1" />').replace(/\n/g, '<br>') }} />
-
             </div>
           </div>
         </div>
