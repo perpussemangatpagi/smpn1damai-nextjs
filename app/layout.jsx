0d946b271d76a049793865f9ed349f263dac5db3
alfisjahri <alfisjahri@gmail.com>
2026-07-01 09:10:11 +0800

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,18 +9,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="id">
       <head>
-        {/* Ini buat manggil Ikon FontAwesome */}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
       </head>
       <body>
-        {/* HEADER / NAVBAR UTAMA */}
-        <header className="header-container glass">
-            <div className="nav-brand">
-                <div className="title">SMPN 1 Damai</div>
-            </div>
-        </header>
-
-        {/* ISI HALAMAN BAKAL MUNCUL DI SINI */}
         {children} 
       </body>
     </html>
