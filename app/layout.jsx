9c5bf5eff9d18a9087eb359140e6deec3400d018
alfisjahri <alfisjahri@gmail.com>
2026-07-01 18:39:04 +0800

    add Open Graph Meta Tags

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,23 @@
+export const metadata = {
+  title: 'SMPN 1 Damai | Official Website',
+  description: 'Membentuk generasi unggul, beradab, dan melek teknologi untuk masa depan gemilang.',
+  // 🔥 Racikan biar muncul Card Info pas di-share ke WA / Sosmed
+  openGraph: {
+    title: 'SMPN 1 Damai | Official Website',
+    description: 'Membentuk generasi unggul, beradab, dan melek teknologi untuk masa depan gemilang.',
+    url: 'https://smpn1damai.web.id', // ganti pakai domain asli lu nanti
+    siteName: 'SMPN 1 Damai',
+    images: [
+      {
+        url: 'https://raw.githubusercontent.com/perpussemangatpagi/smpn1damai-nextjs/main/public/logo_sekolah%20(1).png', // Link gambar thumbnail-nya
+        width: 1200,
+        height: 630,
+        alt: 'Logo Resmi SMPN 1 Damai',
+      },
+    ],
+    type: 'website',
+  },
+};
 import './globals.css'
 
 // Di sinilah KTP Google lu sekarang ditanam secara otomatis sama Next.js!
